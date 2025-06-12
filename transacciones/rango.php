8999363d4a33f80f23885bd9243ed144aaca6010
Julián <julianlopezcastillo12@gmail.com>
2025-06-12 16:55:11 -0400

    mejoras

--- transacciones/rango.php
+++ transacciones/rango.php
@@ -313,7 +313,7 @@ function validateDate($date, $format = 'Y-m-d') {
     <link rel="stylesheet" href="../assets/css/sidebar.css">
     <link rel="stylesheet" href="../assets/css/rango.css">
     <link rel="stylesheet" href="../assets/css/pdf-export.css">
-   link rel="stylesheet" href="../assets/css/pdf-export-individual,css"> 
+    <link rel="stylesheet" href="../assets/css/pdf-export-individual.css"> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 <body>
