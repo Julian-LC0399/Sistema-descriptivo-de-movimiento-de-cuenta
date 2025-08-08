a3f1d703d558513cf1145b022967710d9f6adc19
Julián <julianlopezcastillo12@gmail.com>
2025-08-08 15:52:45 -0400

    mejora

--- auditoria/estados.php
+++ auditoria/estados.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../includes/config.php';
 require_once __DIR__ . '/../includes/database.php';
 requireLogin();
 
-$tituloPagina = "Histórico de estados de cuentas";
+$tituloPagina = "Histórico de estados de cuenta";
 
 // Configuración de paginación
 $registrosPorPagina = 10;
